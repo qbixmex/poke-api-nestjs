0625637e4ee62ba7c4dc81905f38cbcb266dfe13
Daniel González Briseño <qbixmex@gmail.com>
2022-09-06 16:49:06 -0500

    08 Save Pokemon in Database

--- src/pokemons/pokemons.service.ts
+++ src/pokemons/pokemons.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreatePokemonsDto } from './dto/create-pokemons.dto';
 import { UpdatePokemonDto } from './dto/update-pokemons.dto';
+import { Pokemon } from './entities/pokemon.entity';
 
 @Injectable()
 export class PokemonsService {  
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) {}
 
   findAll() {
     return `This action returns all pokemon`;
@@ -13,9 +21,15 @@ export class PokemonsService {
     return `This action returns a #${id} pokemon`;
   }
 
-  create(createPokemonDto: CreatePokemonsDto) {
-    createPokemonDto.name = createPokemonDto.name.toLowerCase();
-    return createPokemonDto;
+  async create(createPokemonDto: CreatePokemonsDto) {
+
+    const pokemon = await this.pokemonModel.create({
+      no: createPokemonDto.no,
+      name: createPokemonDto.name.toLocaleLowerCase()
+    });
+
+    return pokemon;
+
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
