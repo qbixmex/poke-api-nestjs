d3a495bc55144792eb3d96ed792f63f2df943958
Daniel González Briseño <qbixmex@gmail.com>
2022-09-07 13:48:32 -0500

    15 Request from Axios in NestJs

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokemonResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  populate() {
-    return 'This action populate database with new pokemons';
+
+  private readonly axios: AxiosInstance = axios;
+
+  async populate() {
+    const { data } = await this.axios.get<PokemonResponse>('https://pokeapi.co/api/v2/pokemon?limit=1');
+
+    const pokemons = data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no: number = +segments[segments.length - 2];
+      console.log({ name, no });
+    });
+
+    return data.results;
   }
+
 }
